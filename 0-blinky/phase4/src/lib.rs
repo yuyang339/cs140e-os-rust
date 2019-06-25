0b36a54c37c00db2600aaa538abb92efc31c3368
yuyang339 <yuyang339@gmail.com>
2019-06-25 22:10:05 +1000

    phase 4

--- 0-blinky/phase4/src/lib.rs
+++ 0-blinky/phase4/src/lib.rs
@@ -1,9 +1,9 @@
-#![feature(compiler_builtins_lib, lang_items, asm, pointer_methods)]
+#![feature(compiler_builtins_lib, lang_items, asm, panic_fmt, pointer_methods)]
 #![no_builtins]
 #![no_std]
 
-extern crate compiler_builtins;
-
+// extern crate compiler_builtins;
+extern crate compiler_builtins as other_compiler_builtins;
 pub mod lang_items;
 
 const GPIO_BASE: usize = 0x3F000000 + 0x200000;
@@ -22,5 +22,13 @@ fn spin_sleep_ms(ms: usize) {
 #[no_mangle]
 pub unsafe extern "C" fn kmain() {
     // FIXME: STEP 1: Set GPIO Pin 16 as output.
+    *GPIO_FSEL1 |= 1 << 16;
     // FIXME: STEP 2: Continuously set and clear GPIO 16.
+    loop{
+        *GPIO_SET0 |= 1 << 16;
+        spin_sleep_ms(100);
+        *GPIO_CLR0 |= 1 << 16;
+        spin_sleep_ms(100);
+    }
+
 }
